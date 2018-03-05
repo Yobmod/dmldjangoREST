c6d12f8d65df3be4020ae9f4b769e59bbe448d3b
yobmod <yobmod@gmail.com>
2018-03-05 23:09:57 +0000

    add typing

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -1,5 +1,5 @@
-
 import os
+from typing import List
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -14,34 +14,34 @@ SECRET_KEY = 'sm@g)(fbwdh5wc*xe@j++m9rh^uza5se9a57c5ptwkg*b@ki0x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[str] = []
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+	'django.contrib.admin',
 	'django.contrib.admindocs',
-    'django.contrib.auth',
+	'django.contrib.auth',
 	'django.contrib.sites',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
 	'django.contrib.humanize',
-    'django.contrib.staticfiles',
+	'django.contrib.staticfiles',
 
-    # third party
+	# third party
 	'django_extensions',
 	'rest_framework',
-    'crispy_forms',
-    'markdown_deux',
-    'pagedown',
+	'crispy_forms',
+	'markdown_deux',
+	'pagedown',
 	'treebeard',
 
-    # local apps
-    'comments',
+	# local apps
+	'comments',
 	'accounts',
-    'posts',
+	'posts',
 	'charts',
 	'trials',
 
@@ -50,54 +50,53 @@ INSTALLED_APPS = [
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-    	'rest_framework.renderers.BrowsableAPIRenderer',
-    ),
-    #'DEFAULT_PARSER_CLASSES': (
-        #'rest_framework.parsers.JSONParser',
-    #),
+	'DEFAULT_RENDERER_CLASSES': (
+		'rest_framework.renderers.JSONRenderer',
+		'rest_framework.renderers.BrowsableAPIRenderer',
+	),
+	# 'DEFAULT_PARSER_CLASSES': ('rest_framework.parsers.JSONParser',
+	# ),
 	'DEFAULT_AUTHENTICATION_CLASSES': (
 		'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-    	'rest_framework.authentication.SessionAuthentication',
-    	#'rest_framework.authentication.BasicAuthentication',
+		'rest_framework.authentication.SessionAuthentication',
+		# 'rest_framework.authentication.BasicAuthentication',
 	),
 	'DEFAULT_PERMISSION_CLASSES': (
-		 #'rest_framework.permissions.AllowAny', #all views will be this unless has permission class added
-		 'rest_framework.permissions.IsAuthenticated',
-		 #'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+		# 'rest_framework.permissions.AllowAny', #all views will be this unless has permission class added
+		'rest_framework.permissions.IsAuthenticated',
+		# 'rest_framework.permissions.IsAuthenticatedOrReadOnly',
 	)
 }
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.middleware.security.SecurityMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 LOGIN_URL = "/login/"
 ROOT_URLCONF = 'blog.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+	{
+		'BACKEND': 'django.template.backends.django.DjangoTemplates',
+		'DIRS': [os.path.join(BASE_DIR, 'templates')],
+		'APP_DIRS': True,
+		'OPTIONS': {
+			'context_processors': [
+				'django.template.context_processors.debug',
+				'django.template.context_processors.request',
+				'django.contrib.auth.context_processors.auth',
+				'django.contrib.messages.context_processors.messages',
 				"django.template.context_processors.i18n",
 				"django.template.context_processors.media",
-            ],
-        },
-    },
+			],
+		},
+	},
 ]
 
 WSGI_APPLICATION = 'blog.wsgi.application'
@@ -107,10 +106,10 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':  os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	}
 }
 
 
@@ -118,18 +117,18 @@ DATABASES = {
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+	{
+		'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+	},
 ]
 
 
@@ -154,8 +153,8 @@ STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    #'/var/www/static/',
+	os.path.join(BASE_DIR, "static"),
+	# '/var/www/static/',
 ]
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
--- src/charts/views.py
+++ src/charts/views.py
@@ -1,18 +1,20 @@
 from django.contrib.auth import get_user_model
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views.generic import View
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from typing import Any, List, Optional, Dict, Union
+
 
 User = get_user_model()
 
 
-class HomeView(View):   #use context in html js tags
+class HomeView(View):   # use context in html js tags
 	def get(self, request, *args, **kwargs):
-		data = {"customers": User.objects.all().count(), } #can use database info as the data
+		data = {"customers": User.objects.all().count(), }  # can use database info as the data
 		context = {'data': data}
 		return render(request, 'charts/charts_main.html', context)
 
@@ -24,7 +26,7 @@ class HomeView(View):   #use context in html js tags
 # 	return JsonResponse(data) # http response
 
 
-class ChartView(View):   #use context in html js tags
+class ChartView(View):   # use context in html js tags
 	def get(self, request, *args, **kwargs):
 		return render(request, 'charts/charts.html')
 
@@ -35,8 +37,8 @@ class ChartData(APIView):
 
 	def get(self, request, format=None):
 		qs_count = User.objects.all().count()
-		labels = ["Users", "Blue", "Yellow", "Green", "Purple", "Orange"]  #x values
-		default_items = [qs_count, 23, 2, 3, 12, 2]							#y values
+		labels = ["Users", "Blue", "Yellow", "Green", "Purple", "Orange"]  # x values
+		default_items = [qs_count, 23, 2, 3, 12, 2]							# y values
 		colour_background = [
 			'rgba(255, 99, 132, 0.2)',
 			'rgba(54, 162, 235, 0.2)',
@@ -54,15 +56,14 @@ class ChartData(APIView):
 			'rgba(255, 159, 64, 1)'
 		]
 		data = {
-				"labels": labels,				#API veiw, data json object, access by data.key
-				"default": default_items,
-				"colour_background": colour_background,
-				"colour_border": colour_border,
-		}
+			"labels": labels,				# API veiw, data json object, access by data.key
+			"default": default_items,
+			"colour_background": colour_background,
+			"colour_border": colour_border, }
 		return Response(data)
 
 
-class ChartViewTwo(View):   #use context in html js tags
+class ChartViewTwo(View):   # use context in html js tags
 	def get(self, request, *args, **kwargs):
 		return render(request, 'charts/charts2.html')
 
@@ -70,70 +71,77 @@ class ChartViewTwo(View):   #use context in html js tags
 class ChartDataTwo(APIView):
 	authentication_classes = []
 	permission_classes = []
+
 	def get(self, request, format=None):
 		qs_count = User.objects.all().count()
 		labels = ["Users", "Blue", "Yellow", "Green", "Purple", "Orange"]
 		default_items = [qs_count, 23, 2, 3, 12, 2]
 		data = {
-				"labels": labels,
-				"default": default_items,		}
+			"labels": labels,
+			"default": default_items, }
 		return Response(data)
 
 
-class ChartViewLine(View):   #use context in html js tags
+class ChartViewLine(View):   # use context in html js tags
 	def get(self, request, *args, **kwargs):
 		return render(request, 'charts/charts_line.html')
+
 
 class ChartDataLineOne(APIView):
 	authentication_classes = []
 	permission_classes = []
+
 	def get(self, request, format=None):
 		labels = [0, 1, 2, 3, 4, 5]
 		line1 = [89, 43, 2, 3, 22, 2]
 		line2 = [9, 3, 12, 43, 112, 32]
 		data = {
-				"labels": labels,
-				"line1": line1,
-				"line2": line2,			}
+			"labels": labels,
+			"line1": line1,
+			"line2": line2, }
 		return Response(data)
 
-#from rest_framework.views import RetrieveAPIView
-class ChartDataLineTwo(APIView): #scatter takes list of dicts
+
+# from rest_framework.views import RetrieveAPIView
+class ChartDataLineTwo(APIView):  # scatter takes list of dicts
 	authentication_classes = []
 	permission_classes = []
+
 	def get(self, request, format=None):
 		line1 = "[{x:-10, y:10}, {x:7, y:5}, {x:-3, y:7}, {x:-8, y:9}, {x:6, y:6}, {x:-5, y:8}]"
 		line2 = "[{x:0, y:-10}, {x:7, y:2}, {x:-3, y:-7},{x:-5, y:-9}, {x:-15, y:-1}, {x:-1, y:-7}]"
 		line3 = "[{x:-10, y:-10}, {x:-7, y:-2}, {x:-3, y:0},{x:5, y:9}, {x:15, y:1}, {x:16, y:7}]"
 
 		data = {
-				#"labels": labels,
-				"line1": line1,
-				"line2": line2,
-				"line3": line3,		}
+			# "labels": labels,
+			"line1": line1,
+			"line2": line2,
+			"line3": line3, }
 		return Response(data)
-		#return JsonResponse(data)
+		#  return JsonResponse(data)
 
-class ChartViewBarFunnel(View):   #use context in html js tags
+
+class ChartViewBarFunnel(View):   # use context in html js tags
 	def get(self, request, *args, **kwargs):
 		return render(request, 'charts/chart_bar_funnel.html')
 
 
-class ChartViewGamma(View):   #use context in html js tags
+class ChartViewGamma(View):   # use context in html js tags
 	def get(self, request, *args, **kwargs):
 		return render(request, 'charts/chart_gamma.html')
+
 
 class ChartDataGamma(APIView):
 	authentication_classes = []
 	permission_classes = []
 
-	def get(self, request, format=None):
-		x = [2, 4, 6, 8, 10]  #x values
-		y = [10, 20, 30, 40, 50]		#y values
+	def get(self, request: HttpRequest, format: Any=None) -> JsonResponse:
+		x = [2, 4, 6, 8, 10]  # x values
+		y = [10, 20, 30, 40, 50]		# y values
 		y_err = [1, 2, 3, 4, 5]
-		clr_fill = []
-		clr_bord = []
-		pnt_size = []
+		clr_fill = ["red", "pink"]
+		clr_bord = ["red", "pink"]
+		pnt_size = [2]
 		pnt_shape = "xxx"
 		line1 = dict(zip(x, y))
 		line1_d = "["
@@ -149,14 +157,14 @@ class ChartDataGamma(APIView):
 			line1_ld.append(coord_d)
 
 		data = {
-				"x": x,				#API veiw, data json object, access by data.key
-				"y": y,
-				"y_err": y_err,
-				"clr_fill": clr_fill,
-				"clr_bord": clr_bord,
-				"pnt_size": pnt_size,
-				"pnt_shape": pnt_shape,
-				"line1_d": line1_d,
-				"line1_ld": line1_ld,
+			"x": x,				# API veiw, data json object, access by data.key
+			"y": y,
+			"y_err": y_err,
+			"clr_fill": clr_fill,
+			"clr_bord": clr_bord,
+			"pnt_size": pnt_size,
+			"pnt_shape": pnt_shape,
+			"line1_d": line1_d,
+			"line1_ld": line1_ld,
 		}
 		return Response(data)
